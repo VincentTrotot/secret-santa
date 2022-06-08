4b7eea2b44c39db918df405cf9b2bf65a804f2da
Vincent TROTOT <vincent.trotot@icloud.com>
2022-06-08 09:24:37 +0200

    👌🏼 IMPROVE: utilisation de Faker pour les fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use Faker\Factory;
 use App\Entity\Utilisateur;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -12,22 +13,26 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $utilisateurs = new ArrayCollection();
+        $faker = Factory::create('fr_FR');
 
-        for ($i = 0; $i < 14; $i++) {
+        for ($i = 0; $i < 11; $i++) {
             $utilisateur = new Utilisateur();
-            $utilisateur->setPseudo('utilisateur' . $i);
-            $utilisateur->setNom('Nom' . $i);
-            $utilisateur->setPrenom('Prenom' . $i);
-            $utilisateur->setPassword('password' . $i);
+            $nom = $faker->lastName;
+            $prenom = $faker->firstName;
+            $utilisateur->setPseudo($prenom . '.' . $nom);
+            $utilisateur->setNom($nom);
+            $utilisateur->setPrenom($prenom);
+            $utilisateur->setPassword($faker->password);
             $utilisateur->setRoles(['ROLE_USER']);
-            $utilisateur->setDateDeNaissance(new \DateTime('now'));
+            $utilisateur->setDateDeNaissance($faker->dateTimeBetween('-45 years', '-20 years'));
             $utilisateurs->add($utilisateur);
         }
 
-        $utilisateurs[2]->addUtilisateursInterdit($utilisateurs[3]);
-        $utilisateurs[2]->addUtilisateursInterdit($utilisateurs[4]);
+        $utilisateurs[1]->addUtilisateursInterdit($utilisateurs[2]);
+        $utilisateurs[3]->addUtilisateursInterdit($utilisateurs[4]);
+        $utilisateurs[5]->addUtilisateursInterdit($utilisateurs[6]);
         $utilisateurs[7]->addUtilisateursInterdit($utilisateurs[8]);
-        $utilisateurs[11]->addUtilisateursInterdit($utilisateurs[12]);
+        $utilisateurs[9]->addUtilisateursInterdit($utilisateurs[10]);
 
         foreach ($utilisateurs as $utilisateur) {
             $manager->persist($utilisateur);
