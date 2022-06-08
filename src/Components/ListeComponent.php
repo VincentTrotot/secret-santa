eb0675e9743d4f45812a2b97c442b206d0676ef7
Vincent TROTOT <vincent.trotot@icloud.com>
2022-06-08 21:40:30 +0200

    📦 NEW: utilisation de TwigComponent pour afficher les listes de souhait

---
+++ src/Components/ListeComponent.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Components;
+
+use App\Entity\Utilisateur;
+use App\Repository\UtilisateurRepository;
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent('liste')]
+class ListeComponent
+{
+    public int $id;
+
+    public function __construct(
+        private UtilisateurRepository $utilisateurRepository,
+    ) {
+    }
+
+    public function getUtilisateur(): Utilisateur
+    {
+        return $this->utilisateurRepository->find($this->id);
+    }
+}
