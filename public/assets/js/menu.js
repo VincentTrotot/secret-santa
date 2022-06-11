c4ca256ea55646ed043f1c8b91bdb44b8da609ec
Vincent TROTOT <vincent.trotot@icloud.com>
2022-06-11 19:33:56 +0200

    🐛 FIX: fermeture du menu au clic en dehors

--- public/assets/js/menu.js
+++ public/assets/js/menu.js
@@ -1,3 +1,7 @@
 function openMenu() {
     document.getElementById("menu")?.classList.toggle("menu-open");
 }
+
+function closeMenu() {
+    document.getElementById("menu")?.classList.remove("menu-open");
+}
