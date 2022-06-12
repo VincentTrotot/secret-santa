e3258a51c69ace296903ebb298b71f227f759a4e
Vincent TROTOT <vincent.trotot@icloud.com>
2022-06-12 14:25:56 +0200

    🐛 FIX: remplacement de faker

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -26,7 +26,9 @@ class AppFixtures extends Fixture
             $utilisateur = new Utilisateur();
             $nom = $faker->lastName;
             $prenom = $faker->firstName;
-            $utilisateur->setPseudo(mb_strtolower($prenom . '.' . $nom));
+            $utilisateur->setPseudo(
+                Utilisateur::remove_accents(mb_strtolower($prenom . '.' . $nom))
+            );
             $utilisateur->setNom($nom);
             $utilisateur->setPrenom($prenom);
             $utilisateur->setPassword($this->passwordHasher->hashPassword(
